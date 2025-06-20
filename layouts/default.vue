a85852315a2505eef4ce6b1aa103bd9576008925
Ronil29 <santosronilearon@gmail.com>
2025-06-20 17:47:45 +0800

    Added login Page

--- layouts/default.vue
+++ layouts/default.vue
@@ -106,7 +106,11 @@ export default {
           icon: 'mdi-chart-bubble',
           title: 'Inspire',
           to: '/inspire'
-        }
+        },
+        
+      
+        
+        
       ],
       miniVariant: false,
       right: true,
